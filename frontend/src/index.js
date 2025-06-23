5b03a1aff0af66eaccbd283e7033fc69f1d0c5c3
lakshay18bansal <lakshay810bansal@gmail.com>
2025-06-23 15:56:17 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
